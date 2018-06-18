6ab8e0061bdf77e496cd005b4d4e05d454263a0a
Waleed Hamza <hamzah.waleed@gmail.com>
2018-06-18 16:30:28 -0400

    PostHeroku

--- controller/dataController.js
+++ controller/dataController.js
@@ -2,8 +2,6 @@ var express = require('express');
 
 var router = express.Router();
 
-// var zones = require('../models/zones.js')
-
 router.get('/', (req, res) => {
     // console.log('inside the get route')
     console.log(req, res)
