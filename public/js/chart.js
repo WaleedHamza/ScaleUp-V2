0d67be5636b2f98f35409b04e1b00fb89b7b81d0
Waleed Hamza <hamzah.waleed@gmail.com>
2018-06-18 16:23:48 -0400

    preHeroku

--- public/js/chart.js
+++ public/js/chart.js
@@ -4,8 +4,8 @@
 // Could be broken into a different js file, maybe app.js
 $(document).ready(function(){
   $.ajax({
-    url: "http://localhost:8080/api/data",
-    // url: "https://appname.herokuapp.com/api/data",
+    // url: "http://localhost:8080/api/data",
+    url: "https://sleepy-river-20789.herokuapp.com/api/data",
     // url: "http://" + window.location.hostname + ":8080/api/data",
     method: "GET",
     dataType: 'json',
